e8a84b10461b6d003a90f0e2964af6fd5125e205
Blake <blake.huebner3@gmail.com>
2021-08-10 18:27:27 -0500

    wrote getFeasibleBatches function

--- rafs_instance/__init__.py
+++ rafs_instance/__init__.py
@@ -1,6 +1,7 @@
 import xml.etree.cElementTree as ET
 import numpy as np
 import copy
+import pandas as pd
 
 class PodLayout:
     def __init__(self, tierCount= None, tierHeight= None, humanCount= None, humanRad= None, humanMaxAcc= None, humanMaxDec= None,
@@ -590,14 +591,33 @@ class Warehouse:
         cobotCapacity = self.Bots['0'].Capacity
 
 
-        for i in range(len(self.openOrders)):
-            print(i)
+        # creating table with weights of orders
+        weights = []
+        orders = []
+        for i in range(len(self.Orders)):
+            weights.append(self.Orders[i].totalWeight)
+            orders.append(i)
 
+        weight_table = pd.DataFrame({'orders':orders, 'weights':weights})
+        feasibleBatchesList = []
+        cobotCapicity = self.Bots['0'].Capactiy
 
+        def combinations(target, data):
+            for i in range(len(data)):
+                new_target = copy.copy(target)
+                new_data = copy.copy(data)
+                new_target.append(data[i])
+                new_data = data[i + 1:]
+                weights = weight_table.loc[new_target]["weights"]
+                if weights.sum() < cobotCapicity:
+                    if new_target not in feasibleBatchesList:
+                        feasibleBatchesList.append(new_target)
+                else:
+                    new_target = new_target[:-1]
+                combinations(new_target, new_data)
 
-            feasibleBatchesDict[i] = cobotCapacity
-
-
+        target = []
+        combinations(target, weight_table['orders'])
 
             #stationCopy = copy.deepcopy(station)
             # calls the name of the packing station
